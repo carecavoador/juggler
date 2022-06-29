b4e8530e7b505bb3eb74accaafc5947ff4e3bfba
Careca Voador <e.rodrigo@outlook.com>
2022-06-29 11:44:59 -0300

    Continuando...

--- job.py
+++ job.py
@@ -4,7 +4,16 @@ import re
 from pathlib import Path
 from PyPDF2 import PdfFileReader
 
-from osnumber import guess_os_number
+
+def guess_os_number(filename: str) -> tuple[int, int]:
+    """Tries to guess the OS Number and Version from a given string."""
+
+    os_pattern = r"(\d{4,}).*[vV](\d+)"  # regex to match OS Number and Version
+    _os_number = re.search(os_pattern, filename)
+    if _os_number:
+        os_number = _os_number.group(1)
+        os_version = _os_number.group(2)
+        return (os_number, os_version)
 
 
 class Job:
--- juggler.py
+++ juggler.py
@@ -2,39 +2,38 @@
 # pylint: disable=C0103
 """Juggler Bot. Does the hard work for you!"""
 import os
+import json
+
 from pathlib import Path
-from typing import NamedTuple
 from shutil import copy, move
 from datetime import date, datetime
 
 from PyPDF2 import PdfFileReader
 
-from job import Job
-from osnumber import guess_os_number
-from paper_sizes import PaperSheet, A4_PAPER, A3_PAPER, ROLL_PAPER
+from job import Job, guess_os_number
 from juntapdf.juntapdf import merge_pdfs
 
+
 # CONFIGS ---------------------------------------------------------------------
+CONFIG = json.load(open("config.json", "r", encoding="utf-8"))
+
+# Directories.
 ROOT = Path(__file__).parent
-
+CONFIG_DIRS = CONFIG["directories"]
 # Jobs directory.
-JOBS_DIR = Path(ROOT, r"arquivos")
-
-# Layouts input dir and files.
-LAYOUTS_DIR = Path(ROOT, r"arquivos\layouts")
+JOBS_DIR = Path(CONFIG_DIRS["jobs"])
+# Layouts directories.
+LAYOUTS_DIR = Path(CONFIG_DIRS["layouts-in"])
 LAYOUTS_DONE_DIR = Path(LAYOUTS_DIR, "Baixados")
-LAYOUT_FILES = [Path(LAYOUTS_DIR, layout) for layout in os.listdir(LAYOUTS_DIR)]
-LAYOUT_PAPER_SIZES = [A4_PAPER, A3_PAPER, ROLL_PAPER]
-
-# Proofs input dir and files.
-PROOFS_DIR = Path(ROOT, r"arquivos\digitais")
+# Proofs directories.
+PROOFS_DIR = Path(CONFIG_DIRS["proofs-in"])
 PROOFS_DONE_DIR = Path(PROOFS_DIR, "Baixados")
-PROOF_FILES = [Path(PROOFS_DIR, digital) for digital in os.listdir(PROOFS_DIR)]
+# Output directories.
+LAYOUTS_OUT = Path(CONFIG_DIRS["layouts-out"])
+PROOFS_OUT = Path(CONFIG_DIRS["proofs-out"])
 
-# Output dirs.
-OUTPUT_DIR = Path(ROOT, r"arquivos\saida")
-LAYOUTS_OUT = Path(OUTPUT_DIR, "Layouts")
-PROOFS_OUT = Path(OUTPUT_DIR, "Provas Digitais")
+# Layout paper sizes.
+LAYOUT_SIZES = CONFIG["layout-sizes"]
 
 # Formated current date and time.
 TODAY = date.today().strftime("%d-%m-%Y")
@@ -42,17 +41,9 @@ NOW = datetime.now().strftime("_%H-%M-%S")
 # -----------------------------------------------------------------------------
 
 
-class LayoutSizes(NamedTuple):
-    """Printouts separated by sizes."""
-
-    a4: list
-    a3: list
-    roll: list
-
-
 def sort_by_size(
-    printouts_list: list[Path], sizes: list[PaperSheet]
-) -> list[tuple[PaperSheet, list[Path]]]:
+    printouts_list: list[Path], paper_sizes: dict
+) -> list[tuple[str, list[Path]]]:
     """Reads a list of PDF files and returns a tuple of lists with the files
     separated by paper sizes."""
 
@@ -63,7 +54,7 @@ def sort_by_size(
     separated_layouts = []
 
     for printout in printouts_list:
-        for paper_size in sizes:
+        for size in paper_sizes.keys():
             with open(printout, "rb") as file:
                 reader = PdfFileReader(file)
                 page = reader.pages[0]
@@ -71,10 +62,14 @@ def sort_by_size(
                 height = round(float(page.mediabox.height) / pdf_res_to_mm)
 
                 # Goes into an A4 sheet.
-                if (width <= paper_size.width and height <= paper_size.height) or (
-                    width <= paper_size.height and height <= paper_size.width
+                if (
+                    width <= paper_sizes[size]["width"]
+                    and height <= paper_sizes[size]["height"]
+                ) or (
+                    width <= paper_sizes[size]["height"]
+                    and height <= paper_sizes[size]["width"]
                 ):
-                    separated_layouts.append((paper_size, printout))
+                    separated_layouts.append((size, printout))
                     break
 
     return separated_layouts
@@ -97,7 +92,7 @@ def prompt_user(prompt: str) -> bool:
             print("Opção inválida.")
 
 
-def download_files(job: Job, source: list[Path], output: Path, done_dir: Path) -> None:
+def download_files(files_to_download: list[Path], output: Path, done_dir: Path) -> None:
     """Retrieves Job files to output destination and copies done files to done_dir."""
 
     if not done_dir.exists():
@@ -105,12 +100,6 @@ def download_files(job: Job, source: list[Path], output: Path, done_dir: Path) -
 
     if not output.exists():
         os.mkdir(output)
-
-    files_to_download = [
-        file
-        for file in source
-        if guess_os_number(file.name) == (job.os_number, job.os_version)
-    ]
 
     for file in files_to_download:
 
@@ -130,7 +119,7 @@ def download_files(job: Job, source: list[Path], output: Path, done_dir: Path) -
                 copy(file, file_done)
                 print(f"Arquivo salvo como {file_done}.")
             else:
-                files_to_download.pop(file)
+                files_to_download.pop(files_to_download.index(file))
                 continue
 
         # Checks if file is not already in the output directory and copies it.
@@ -176,29 +165,41 @@ def work() -> None:
     # Looks for Jobs' files in Layouts directory and Proofs directory and downloads if any.
     for current_job in jobs_todo:
         if current_job.needs_layout:
-            layout_files = download_files(
-                current_job, LAYOUT_FILES, LAYOUTS_OUT, LAYOUTS_DONE_DIR
-            )
-            if layout_files:
+            job_layout_files = [
+                Path(LAYOUTS_DIR, file)
+                for file in os.listdir(LAYOUTS_DIR)
+                if guess_os_number(file)
+                == (current_job.os_number, current_job.os_version)
+            ]
+            if job_layout_files:
+                layouts_downloaded = download_files(
+                    job_layout_files, LAYOUTS_OUT, LAYOUTS_DONE_DIR
+                )
                 print(
-                    f"{len(layout_files)} arquivos carregados com sucesso para {current_job}."
+                    f"{len(layouts_downloaded)} arquivos de Print Layout carregados com sucesso para {current_job}."
                 )
             else:
                 print(
-                    f"Não foi possível localizar arquivos de print Layout para {current_job}."
+                    f"Não foi possível localizar arquivos de Print Layout para {current_job}."
                 )
 
         if current_job.needs_proof:
-            proof_files = download_files(
-                current_job, PROOF_FILES, PROOFS_OUT, PROOFS_DONE_DIR
-            )
-            if proof_files:
+            job_proof_files = [
+                Path(PROOFS_DIR, file)
+                for file in os.listdir(PROOFS_DIR)
+                if guess_os_number(file)
+                == (current_job.os_number, current_job.os_version)
+            ]
+            if job_proof_files:
+                proofs_downloaded = download_files(
+                    job_proof_files, PROOFS_OUT, PROOFS_DONE_DIR
+                )
                 print(
-                    f"{len(proof_files)} arquivos carregados com sucesso para {current_job}."
+                    f"{len(proofs_downloaded)} arquivos de Prova Digital carregados com sucesso para {current_job}."
                 )
             else:
                 print(
-                    f"Não foi possível localizar arquivos de print Layout para {current_job}."
+                    f"Não foi possível localizar arquivos de Prova Digital para {current_job}."
                 )
 
     # Organize layouts by size.
@@ -208,22 +209,24 @@ def work() -> None:
         if Path(layout).suffix.lower() == ".pdf"
     ]
 
-    layouts_by_size = sort_by_size(pdf_layouts, LAYOUT_PAPER_SIZES)
+    if pdf_layouts:
 
-    for paper_size in LAYOUT_PAPER_SIZES:
+        layouts_by_size = sort_by_size(pdf_layouts, LAYOUT_SIZES)
 
-        size_path = Path(LAYOUTS_OUT, paper_size.size)
+        for size in LAYOUT_SIZES.keys():
 
-        if not size_path.exists():
-            os.mkdir(size_path)
+            size_path = Path(LAYOUTS_OUT, size)
 
-        layouts = [
-            layout[1] for layout in layouts_by_size if layout[0].size == paper_size.size
-        ]
-        merge_pdfs(layouts, LAYOUTS_OUT.joinpath(f"Layouts {paper_size.size}.pdf"))
+            if not size_path.exists():
+                os.mkdir(size_path)
 
-        for layout in layouts:
-            move(layout, size_path.joinpath(layout.name))
+            layouts = [layout[1] for layout in layouts_by_size if layout[0] == size]
+
+            if layouts:
+                merge_pdfs(layouts, LAYOUTS_OUT.joinpath(f"Layouts {size}.pdf"))
+
+            for layout in layouts:
+                move(layout, size_path.joinpath(layout.name))
 
 
 if __name__ == "__main__":
